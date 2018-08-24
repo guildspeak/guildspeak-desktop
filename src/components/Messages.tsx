14d056e9634ae85111b5b9c1d1ddaa246ed74563
xdk78 <xdk78888@gmail.com>
2018-08-24 13:22:30 +0200

    feat: custom scroll bar in messages

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -9,6 +9,15 @@ const Wrapper = styled.div`
 	flex-direction: column;
   flex: 1;
   overflow: auto;
+  &::-webkit-scrollbar {
+    width: 12px;
+    background-color:  #2e2e38;
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 8px;
+    -webkit-box-shadow: inset 0 0 1px rgba(0,0,0,.3);
+    background-color:  #27272f;
+  }
 `
 
 const MESSAGE_SUBSCRIPTION = gql`
