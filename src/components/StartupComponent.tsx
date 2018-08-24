8e3b66ddcbd40fb5096956048875c0b1a75bbe20
xdk78 <xdk78888@gmail.com>
2018-08-24 12:46:15 +0200

    fix: issues in startup component

--- src/components/StartupComponent.tsx
+++ src/components/StartupComponent.tsx
@@ -2,10 +2,22 @@ import * as React from 'react'
 import Application from './Application'
 import { withRouter } from 'react-router-dom'
 
-class StartupComponent extends React.Component<{ token: string, history: any }, {}> {
-  render() {
+class StartupComponent extends React.Component<{ token: string, history: any }, { logged: boolean }> {
+  state = {
+    logged: false
+  }
+
+  componentDidMount() {
     if (!this.props.token) {
+      this.setState({ logged: true })
       this.props.history.push('/login')
+    } else {
+      this.setState({ logged: false })
+    }
+  }
+
+  render() {
+    if (this.state.logged) {
       return (<p> redirecting to login</p>)
     }
     return <Application />
