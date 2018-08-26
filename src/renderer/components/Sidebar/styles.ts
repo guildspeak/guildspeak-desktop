349bf6837dcbd1ef83832d1b5a682a8800784125
Unknown <kacper.potyrala@gmail.com>
2018-08-26 16:59:31 +0200

    Log out button (Not working :D)

--- src/renderer/components/Sidebar/styles.ts
+++ src/renderer/components/Sidebar/styles.ts
@@ -7,4 +7,22 @@ const Wrapper = styled.div`
   flex: 1;
   `
 
-export { Wrapper }
+const LogButton = styled.button`
+  position: relative;
+  display: inline-block;
+  margin: 0.1rem;
+  padding: 0.2rem 0.2rem;
+  background: var(--color-primary-500);
+  color: var(--color-secondary-900);
+  font-family: inherit;
+  font-size: 1rem;
+  font-weight: 300;
+  line-height: normal;
+  border: 0;
+  border-radius: 0.4rem;
+  appearance: none;
+  cursor: pointer;
+  transition: background 250ms, box-shadow 250ms;
+`
+
+export { Wrapper, LogButton }
