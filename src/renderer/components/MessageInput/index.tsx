72a40c1ad164d35dcf9132acb1412362086983ef
xdk78 <xdk78888@gmail.com>
2018-08-27 23:11:37 +0200

    feat: inline input and prepare emoji support

--- src/renderer/components/MessageInput/index.tsx
+++ src/renderer/components/MessageInput/index.tsx
@@ -2,8 +2,20 @@ import * as React from 'react'
 import gql from 'graphql-tag'
 import { Mutation } from 'react-apollo'
 import { Wrapper, Input } from './styles'
-import { EditorState, Editor, DraftHandleValue } from 'draft-js'
-import '../../../../node_modules/draft-js/dist/Draft.css'
+import Editor, { createEditorStateWithText } from 'draft-js-plugins-editor'
+import { EditorState, DraftHandleValue } from 'draft-js'
+import 'draft-js/dist/Draft.css'
+import createSingleLinePlugin from 'draft-js-single-line-plugin'
+import createEmojiPlugin from 'draft-js-emoji-plugin'
+import 'draft-js-emoji-plugin/lib/plugin.css'
+
+const singleLinePlugin = createSingleLinePlugin()
+const emojiPlugin = createEmojiPlugin({
+  useNativeArt: true
+})
+
+const { EmojiSelect, EmojiSuggestions } = emojiPlugin
+const plugins = [singleLinePlugin, emojiPlugin]
 
 const CREATE_MESSAGE = gql`
   mutation createMessage($content: String!, $channelId: ID!) {
@@ -23,23 +35,30 @@ interface Props {
   channelId: string
 }
 
+const StyledEmojiSuggestions = {}
+
 class MessageInput extends React.Component<Props, IState> {
   state = {
-    editorState: EditorState.createEmpty(),
+    editorState: createEditorStateWithText(''),
     content: ''
   }
 
   handleReturn(createMessage): DraftHandleValue {
-    const content = this.state.editorState.getCurrentContent().getPlainText()
-    this.setState({
-      content
-    })
-    createMessage({ variables: { content, channelId: this.props.channelId } })
-    this.setState({
-      content: '',
-      editorState: EditorState.createEmpty()
-    })
-    return 'handled'
+    const content = this.state.editorState
+      .getCurrentContent()
+      .getPlainText()
+      .trim()
+    if (content && content.length > 0) {
+      this.setState({
+        content
+      })
+      createMessage({ variables: { content, channelId: this.props.channelId } })
+      this.setState({
+        content: '',
+        editorState: EditorState.createEmpty()
+      })
+      return 'handled'
+    }
   }
 
   handleChange = editorState => {
@@ -48,21 +67,24 @@ class MessageInput extends React.Component<Props, IState> {
 
   render() {
     return (
-      <Wrapper>
-        <Mutation mutation={CREATE_MESSAGE}>
-          {createMessage => (
+      <Mutation mutation={CREATE_MESSAGE}>
+        {createMessage => (
+          <Wrapper>
             <Input>
               <Editor
+                plugins={plugins}
+                blockRenderMap={singleLinePlugin.blockRenderMap}
                 placeholder="write message..."
                 editorState={this.state.editorState}
                 onChange={this.handleChange}
                 // tslint:disable-next-line:jsx-no-lambda
                 handleReturn={() => this.handleReturn(createMessage)}
               />
+              <EmojiSuggestions style={StyledEmojiSuggestions} />
             </Input>
-          )}
-        </Mutation>
-      </Wrapper>
+          </Wrapper>
+        )}
+      </Mutation>
     )
   }
 }
