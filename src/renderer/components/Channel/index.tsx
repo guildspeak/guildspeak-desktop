063d6222658613a6fb04f382a01c2f16c8c3e48b
SerekKiri <kacper.potyrala@gmail.com>
2018-09-09 00:07:54 +0200

    Add settings button to channels
    TO DO:
    Style this button

--- src/renderer/components/Channel/index.tsx
+++ src/renderer/components/Channel/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { withRouter, RouteProps, RouteComponentProps } from 'react-router-dom'
-import { Wrapper, ChannelName } from './styles'
+import { Wrapper, ChannelName, Button } from './styles'
 
 interface IProps {
   id: string
@@ -21,6 +21,9 @@ class Channel extends React.Component<IProps & RouteComponentProps<RouteProps>> 
         <ChannelName {...this.props} onClick={this.changeChannel}>
           #{this.props.name}
         </ChannelName>
+        <Button className="material-icons">
+          more_vert
+        </Button>
       </Wrapper>
     )
   }
--- src/renderer/components/Channel/styles.ts
+++ src/renderer/components/Channel/styles.ts
@@ -1,17 +1,26 @@
 import styled from 'styled-components'
 
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+  display: flex;
+  justify-items: left;
+  flex-direction: row; 
+`
 
 const ChannelName = styled.div`
   padding: 8px;
   color: #eeeeee;
   font-size: 16px;
-  ${props => ((props as any).channelId === props.id ? 'background: #2d2d31;font-weight: 500;' : '')} &:hover {
+  padding-top: 0.5rem;
+  cursor: pointer;
+  text-transform: none;
+  ${props => ((props as any).channelId === props.id ? 'background: #2d2d31;font-weight: 500;' : '')} &:hover + Button {
     color: #fff;
     background: #2d2d31;
   }
-  cursor: pointer;
-  text-transform: none;
 `
 
-export { Wrapper, ChannelName }
+const Button = styled.div`
+  cursor: pointer;
+`
+
+export { Wrapper, ChannelName, Button }
