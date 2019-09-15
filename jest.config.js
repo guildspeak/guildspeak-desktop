e03407c9e448bf66a6b3ff1a0e956abba24aaab7
xdk78 <xdk78888@gmail.com>
2019-09-15 16:04:10 +0200

    fix: tests with css

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,10 @@ module.exports = {
   transform: {
     '\\.(ts|tsx|js|jsx)?$': 'babel-jest'
   },
+  moduleNameMapper: {
+    '\\.(scss|sass|css)$': 'identity-obj-proxy'
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testPathIgnorePatterns: ['/node_modules/', '/static/'],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'] // setupFiles before the tests are ran
